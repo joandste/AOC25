16a50676f873f8f9e69abd82920d636a9e7f5248
Jonathan <joandste@abo.fi>
2025-12-11 22:56:07 +0200

    clean

--- src/aoc/day3.scm
+++ src/aoc/day3.scm
@@ -1,9 +1,10 @@
 (define input (read-port-to-string (open-input-file "input")))
 
-(define parsed-input (reverse (drop (reverse (map string->list (split-many input "\n"))) 1)))
+(define parsed-input (map string->list (split-many input "\n")))
 
 (define (concat-integers a b)
-  (string->int (string-append (int->string a) (int->string b))))
+  (string->int (string-append (int->string a)
+			      (int->string b))))
 
 (define (get-battery coll acc)
   (if (empty? (cdr coll)) acc
@@ -11,6 +12,7 @@
 				  (apply max (cdr coll)))))
       (get-battery (cdr coll)
 		   (if (< acc check) check acc)))))
+
 
 
 (displayln (reduce + 0 (map (λ (coll) (get-battery (map char->number coll) 0)) parsed-input)))
